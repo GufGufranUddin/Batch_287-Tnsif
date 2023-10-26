579acd06e146ba488c7b8984125118ef351bfd48
GufGufranUddin <uddingufran039@gmail.com>
2023-10-26 23:53:17 +0530

    First Commit

---
+++ FirstProgram.java
@@ -0,0 +1,18 @@
+
+public class FirstProgram {
+    public static void main(String[] args) {
+
+        String name = "MD GUFRAN UDDIN";
+
+        long regNo = 4018;
+
+        String department = "CSE";
+
+        String collegeName = "Al-Ameen Engineering College";
+
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Department: " + department);
+        System.out.println("College Name: " + collegeName);
+    }
+}
